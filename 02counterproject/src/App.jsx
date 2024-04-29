9d82661a202df0e152b394930a7fcb073c84d172
Ritesh <riteshshami@gmail.com>
2024-04-29 23:31:27 +0530

    useState

---
+++ 02counterproject/src/App.jsx
@@ -0,0 +1,35 @@
+import { useState } from "react";
+
+function App() {
+
+
+  const [counter, setCounter] = useState(0);
+
+
+  const addValue = () => {
+    console.log(`value added: ${Math.random()*10}`)
+    // counter = counter + 1;
+    setCounter(counter+1)
+  }
+
+  const removeValue = () => {
+    console.log(`value added: ${Math.random()*10}`)
+    // counter = counter - 1;
+    setCounter(counter-1)
+  }
+
+  return (
+    <>
+    <h1>Chai aur react</h1>
+    <h2>Counter value: {counter}</h2>
+
+    <button onClick={addValue}>Add Value {counter}</button>
+    <br/>
+    <button onClick={removeValue}>Remove Value {counter}</button>
+    </>
+  )
+}
+
+export default App
+
+// Add method where value will not be zero.
