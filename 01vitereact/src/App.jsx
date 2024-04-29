0a9af26b57b54c85dfa60eb3524e6ec1540b2d68
Ritesh <riteshshami@gmail.com>
2024-04-29 22:57:19 +0530

    Overview of react in depth

---
+++ 01vitereact/src/App.jsx
@@ -0,0 +1,13 @@
+
+function App() {
+
+  const username = "Chai Aur code" 
+
+  return (
+    <h1>
+      Chai Aur Code: React with Vite | Ritesh {username} {/*-> evaluated expression */} 
+    </h1>
+  )
+}
+
+export default App
